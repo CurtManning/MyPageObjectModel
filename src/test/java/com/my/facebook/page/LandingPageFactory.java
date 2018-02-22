9f40f17ecf9e1b6850e0a6df623194d054c81b6a
Curt Manning <cmann4ski@yahoo.com>
2018-02-22 08:01:54 -0800

    LandingPageTest only checks 4 years

--- src/test/java/com/my/facebook/page/LandingPageFactory.java
+++ src/test/java/com/my/facebook/page/LandingPageFactory.java
@@ -1,5 +1,9 @@
 package com.my.facebook.page;
 
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.util.List;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
@@ -8,33 +12,55 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-
 public class LandingPageFactory {
-	
+
 	private static final Logger log = LogManager.getLogger(LandingPageFactory.class.getName());
 	WebDriver driver;
 
-	@FindBy(id="pageTitle")
+	@FindBy(id = "pageTitle")
 	WebElement titleText;
 
-	@FindBy(name="firstname")
+	@FindBy(name = "firstname")
 	WebElement firstName;
-	
-	@FindBy(name="lastname")
+
+	@FindBy(name = "lastname")
 	WebElement lastName;
-	
-	@FindBy(name="reg_email__")
+
+	@FindBy(name = "reg_email__")
 	WebElement mobileOrEmail;
-	
-	@FindBy(name="reg_passwd__")
+
+	@FindBy(name = "reg_passwd__")
 	WebElement password;
-	
-	@FindBy(xpath="//input[@value='1']")
+
+	@FindBy(xpath = "//input[@value='1']")
 	WebElement genderFemale;
 
-	@FindBy(xpath="//input[@value='2']")
+	@FindBy(xpath = "//input[@value='2']")
 	WebElement genderMale;
-	
+
+	@FindBy(name = "websubmit")
+	WebElement clickCreateAccount;
+
+	@FindBy(xpath = "//*[@id='month']/option")
+	List<WebElement> months;
+
+	@FindBy(xpath = "//*[@id='year']/option")
+	List<WebElement> years;
+
+	@FindBy(xpath = "//*[@id='day']/option")
+	List<WebElement> days;
+
+	@FindBy(xpath = "//*[@id='year']/option[[@value='Log out']]")
+	List<WebElement> byear;
+
+	public List<WebElement> getMonths() {
+
+		List<WebElement> elementList = months;
+
+		// System.out.println("Size of the element list is: " + size);
+		return elementList;
+	}
+
 	// Get the title of Login Page
 
 	public String getLoginTitle() {
@@ -48,47 +74,102 @@ public class LandingPageFactory {
 		return firstName.getText();
 	}
 
-
 	public void setFirstName(String first) {
 		log.debug(" setFirstName " + first);
+		firstName.clear();
 		firstName.sendKeys(first);
 	}
-	
+
 	public void setLastName(String last) {
 		log.debug(" setLastName " + last);
+		lastName.clear();
 		lastName.sendKeys(last);
 	}
-	
+
 	public void setMobileOrEmail(String mobileEmail) {
 		log.debug(" setMobileOrEmail " + mobileEmail);
+		mobileOrEmail.clear();
 		mobileOrEmail.sendKeys(mobileEmail);
 	}
-	
+
 	public void setPassword(String passwrd) {
 		log.debug(" setPassword " + passwrd);
+		password.clear();
 		password.sendKeys(passwrd);
 	}
-	
+
 	public void clearPassword() {
 		log.debug(" clearPassword ");
 		password.clear();
 	}
+
+	public void clickBirthMonth(int bMonth) {
+
+		WebElement month = driver.findElement(By.xpath("//*[@id='month']/option[@value='" + bMonth + "']"));
+		// log.debug(" clickMonth " + month);
+		month.click();
+	}
 	
+	public void clickBirthYear(int bYear) {
+		log.debug(" clickBirthYear ");
+		WebElement year = driver.findElement(By.xpath("//*[@id='year']/option[@value='" + bYear + "']"));
+		year.click();
+	}
+	
+
+
+	public void clickDays(int bday) {
+
+		WebElement day = driver.findElement(By.xpath("//*[@id='day']/option[@value='" + bday + "']"));
+		// log.debug(" clickDays " + day);
+		day.click();
+	}
+
 	public void clickGenderFemale() {
 		log.debug(" clickGenderFemale ");
 		genderFemale.click();
 	}
-	
+
 	public void clickGenderMale() {
 		log.debug(" clickGenderMale ");
 		genderMale.click();
 	}
-	
-	
+
 	public LandingPageFactory(WebDriver driver) {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
-	
+
+	public void clickCreateAcct() {
+
+		log.debug(" clickCreateAccount ");
+		clickCreateAccount.click();
+	}
+
+	public List<WebElement> getYears() {
+
+		List<WebElement> elementYearList = years;
+		
+		return elementYearList;
+	}
+
+	public List<WebElement> getDays() {
+
+		List<WebElement> elementDayList = days;
+		// int size = elementDayList.size();
+		// System.out.println("Size of the element list is: " + size);
+		return elementDayList;
+	}
+
+	public boolean isDateValid(int year, int month, int day) {
+
+	    boolean dateIsValid = true;
+	    try {
+	        LocalDate.of(year, month, day);
+	    } catch (DateTimeException e) {
+	        dateIsValid = false;
+	    }
+	    return dateIsValid;
+	}
 
 }
--- src/test/java/com/my/facebook/tests/LandingPageTest.java
+++ src/test/java/com/my/facebook/tests/LandingPageTest.java
@@ -1,11 +1,17 @@
 package com.my.facebook.tests;
 
 
+import java.util.List;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.my.base.TestLandingPageSetup;
@@ -19,41 +25,105 @@ public class LandingPageTest extends TestLandingPageSetup {
 	private WebDriver driver;
 	LandingPageFactory landingPage;
 
+	@DataProvider
+	public Object[][] getData() {
+		return new Object[][] { { "Joe", "Manning", "test@gmail.com", "7777", "7777777?", "M"}, 
+			{ "Sally", "Smith", "ssmith@yahoo.com", "4444", "4444444-", "F"},
+			{ "Carol", "MacDuffy", "surf@sbcglobal.net", "1111", "2222222!", "F"} };
+
+	}
+
 	@BeforeClass
 	public void setUp() {
 	
 		driver = getDriver();
 
 	}
+	
+
 
 	/**
 	 * This test goes to http:www.facebook.com and verifies the login page title.
 	 * @throws InterruptedException 
 	 */
 
-	@Test
-	public void test_Home_Page_Appear_Correct() throws InterruptedException {
+	@Test(dataProvider = "getData")
+	public void test_Home_Page_Appear_Correct(String firstName, String lastName, String email, String password1, String password2, String gender) throws InterruptedException {
 		// Create Login Page object
 		landingPage = new LandingPageFactory(driver);
 		String loginPageTitle = landingPage.getLoginTitle();
-		landingPage.setFirstName("Joe");
-		landingPage.setLastName("Manning");
-		landingPage.setMobileOrEmail("test@gmail.com");
-		landingPage.setPassword("7777");
+		Thread.sleep(1000);
+		landingPage.clickCreateAcct();
 		Thread.sleep(2000);
-		landingPage.clearPassword();
+		landingPage.setFirstName(firstName);
+		Thread.sleep(1000);
+		landingPage.setLastName(lastName);
+		Thread.sleep(1000);
+		landingPage.setMobileOrEmail(email);
+		Thread.sleep(1000);
+		landingPage.setPassword(password1);
 		Thread.sleep(2000);
-		landingPage.setPassword("7777777?");
+		landingPage.setPassword(password2);
 		Thread.sleep(2000);
-		landingPage.clickGenderMale();
+		landingPage.getMonths();
+		if (gender == "M") {
+			landingPage.clickGenderFemale();
+			Thread.sleep(2000);
+			landingPage.clickGenderMale();
+		} else {
+			landingPage.clickGenderMale();
+			Thread.sleep(2000);
+			landingPage.clickGenderFemale();
+		}
+
 		Thread.sleep(2000);
-		landingPage.clickGenderFemale();
-		Thread.sleep(2000);
-		landingPage.clickGenderMale();
-		Thread.sleep(12000);
 
 		Assert.assertTrue(loginPageTitle.contains("Facebook - Log In or Sign Up"));
-		
+	
+	}
+	
+
+	@Test
+	public void test_Birth_Dates () throws InterruptedException {
+		// Create Login Page object
+		landingPage = new LandingPageFactory(driver);
+		String loginPageTitle = landingPage.getLoginTitle();
+		List<WebElement> elementYearList = landingPage.getYears();
+		int size = elementYearList.size();
+		int FOUR_YEARS = 4;
+		for (int i = 1; i < FOUR_YEARS; i++) {   //  replace FOUR_YEARS with size to scroll through everything
+			
+			int intYear = Integer.parseInt(elementYearList.get(i).getText());
+			landingPage.clickBirthYear(intYear);
+			
+			for (int b = 1; b < 12; b++) {
+				landingPage.clickBirthMonth(b); 
+				List<WebElement> elementDays =landingPage.getDays();
+				int sizeDay = elementDays.size();
+	
+				for (int c = 1; c <= sizeDay -1; c++) {
+					landingPage.clickDays(c);
+					if (!landingPage.isDateValid(intYear, b, c)) {
+						Reporter.log("Invalid Date " + b + "-" + c + "-" + intYear, true);
+					}
+				}
+			}
+			
+		}
+		//Assert.assertTrue(numberOfMonths == 13);
+
+		Thread.sleep(2000);
+
+		Assert.assertTrue(loginPageTitle.contains("Facebook - Log In or Sign Up"));
+	
+	}
+
+		@AfterClass
+		public void afterClass( ) {
+			// Close
+			this.driver.close();
+			this.driver = null;
+			
 	}
 	
 }
